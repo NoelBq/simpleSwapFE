65481d43df62918430749154bf4ec05503cc7214
NoelBq <noeliabaquerostaci@gmail.com>
2025-07-20 20:12:07 -0300

    - updates abi to new contract

--- src/types/contracts.ts
+++ src/types/contracts.ts
@@ -3,31 +3,30 @@ import { Address } from "viem";
 // SimpleSwap ABI
 export const SIMPLE_SWAP_ABI = [
   {
-    inputs: [],
-    name: "ReentrancyGuardReentrantCall",
-    type: "error",
+    inputs: [
+      { internalType: "address", name: "_tokenA", type: "address" },
+      { internalType: "address", name: "_tokenB", type: "address" },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
   },
+  { inputs: [], name: "Expired", type: "error" },
+  { inputs: [], name: "IdenticalAddresses", type: "error" },
+  { inputs: [], name: "InsufficientAmountA", type: "error" },
+  { inputs: [], name: "InsufficientAmountB", type: "error" },
+  { inputs: [], name: "InsufficientInputAmount", type: "error" },
+  { inputs: [], name: "InsufficientLiquidity", type: "error" },
+  { inputs: [], name: "InsufficientLiquidityBalance", type: "error" },
+  { inputs: [], name: "InsufficientLiquidityMinted", type: "error" },
+  { inputs: [], name: "InsufficientOutputAmount", type: "error" },
+  { inputs: [], name: "InvalidPath", type: "error" },
+  { inputs: [], name: "NoLiquidity", type: "error" },
+  { inputs: [], name: "ReentrancyGuardReentrantCall", type: "error" },
+  { inputs: [], name: "ZeroAddress", type: "error" },
   {
     anonymous: false,
     inputs: [
-      {
-        indexed: true,
-        internalType: "address",
-        name: "user",
-        type: "address",
-      },
-      {
-        indexed: true,
-        internalType: "address",
-        name: "tokenA",
-        type: "address",
-      },
-      {
-        indexed: true,
-        internalType: "address",
-        name: "tokenB",
-        type: "address",
-      },
+      { indexed: true, internalType: "address", name: "user", type: "address" },
       {
         indexed: false,
         internalType: "uint256",
@@ -53,24 +52,7 @@ export const SIMPLE_SWAP_ABI = [
   {
     anonymous: false,
     inputs: [
-      {
-        indexed: true,
-        internalType: "address",
-        name: "user",
-        type: "address",
-      },
-      {
-        indexed: true,
-        internalType: "address",
-        name: "tokenA",
-        type: "address",
-      },
-      {
-        indexed: true,
-        internalType: "address",
-        name: "tokenB",
-        type: "address",
-      },
+      { indexed: true, internalType: "address", name: "user", type: "address" },
       {
         indexed: false,
         internalType: "uint256",
@@ -96,12 +78,7 @@ export const SIMPLE_SWAP_ABI = [
   {
     anonymous: false,
     inputs: [
-      {
-        indexed: true,
-        internalType: "address",
-        name: "user",
-        type: "address",
-      },
+      { indexed: true, internalType: "address", name: "user", type: "address" },
       {
         indexed: true,
         internalType: "address",
@@ -131,358 +108,136 @@ export const SIMPLE_SWAP_ABI = [
     type: "event",
   },
   {
-    inputs: [],
-    name: "FEE_DENOMINATOR",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "SWAP_FEE",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "",
-        type: "uint256",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
     inputs: [
-      {
-        internalType: "address",
-        name: "tokenA",
-        type: "address",
-      },
-      {
-        internalType: "address",
-        name: "tokenB",
-        type: "address",
-      },
-      {
-        internalType: "uint256",
-        name: "amountADesired",
-        type: "uint256",
-      },
-      {
-        internalType: "uint256",
-        name: "amountBDesired",
-        type: "uint256",
-      },
-      {
-        internalType: "uint256",
-        name: "amountAMin",
-        type: "uint256",
-      },
-      {
-        internalType: "uint256",
-        name: "amountBMin",
-        type: "uint256",
-      },
-      {
-        internalType: "address",
-        name: "to",
-        type: "address",
-      },
-      {
-        internalType: "uint256",
-        name: "deadline",
-        type: "uint256",
-      },
+      { internalType: "address", name: "", type: "address" },
+      { internalType: "address", name: "", type: "address" },
+      { internalType: "uint256", name: "amountADesired", type: "uint256" },
+      { internalType: "uint256", name: "amountBDesired", type: "uint256" },
+      { internalType: "uint256", name: "amountAMin", type: "uint256" },
+      { internalType: "uint256", name: "amountBMin", type: "uint256" },
+      { internalType: "address", name: "to", type: "address" },
+      { internalType: "uint256", name: "deadline", type: "uint256" },
     ],
     name: "addLiquidity",
     outputs: [
-      {
-        internalType: "uint256",
-        name: "amountA",
-        type: "uint256",
-      },
-      {
-        internalType: "uint256",
-        name: "amountB",
-        type: "uint256",
-      },
-      {
-        internalType: "uint256",
-        name: "liquidity",
-        type: "uint256",
-      },
+      { internalType: "uint256", name: "amountA", type: "uint256" },
+      { internalType: "uint256", name: "amountB", type: "uint256" },
+      { internalType: "uint256", name: "liquidity", type: "uint256" },
     ],
     stateMutability: "nonpayable",
     type: "function",
   },
   {
     inputs: [
-      {
-        internalType: "uint256",
-        name: "amountIn",
-        type: "uint256",
-      },
-      {
-        internalType: "uint256",
-        name: "reserveIn",
-        type: "uint256",
-      },
-      {
-        internalType: "uint256",
-        name: "reserveOut",
-        type: "uint256",
-      },
+      { internalType: "uint256", name: "amountIn", type: "uint256" },
+      { internalType: "uint256", name: "reserveIn", type: "uint256" },
+      { internalType: "uint256", name: "reserveOut", type: "uint256" },
     ],
     name: "getAmountOut",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "amountOut",
-        type: "uint256",
-      },
-    ],
+    outputs: [{ internalType: "uint256", name: "amountOut", type: "uint256" }],
     stateMutability: "pure",
     type: "function",
   },
   {
-    inputs: [
-      {
-        internalType: "address",
-        name: "user",
-        type: "address",
-      },
-      {
-        internalType: "address",
-        name: "tokenA",
-        type: "address",
-      },
-      {
-        internalType: "address",
-        name: "tokenB",
-        type: "address",
-      },
-    ],
+    inputs: [{ internalType: "address", name: "user", type: "address" }],
     name: "getLiquidityBalance",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "balance",
-        type: "uint256",
-      },
-    ],
+    outputs: [{ internalType: "uint256", name: "balance", type: "uint256" }],
     stateMutability: "view",
     type: "function",
   },
   {
-    inputs: [
-      {
-        internalType: "address",
-        name: "tokenA",
-        type: "address",
-      },
-      {
-        internalType: "address",
-        name: "tokenB",
-        type: "address",
-      },
-    ],
+    inputs: [],
     name: "getPoolInfo",
     outputs: [
-      {
-        internalType: "uint256",
-        name: "reserveA",
-        type: "uint256",
-      },
-      {
-        internalType: "uint256",
-        name: "reserveB",
-        type: "uint256",
-      },
-      {
-        internalType: "uint256",
-        name: "totalSupply",
-        type: "uint256",
-      },
+      { internalType: "uint256", name: "_reserveA", type: "uint256" },
+      { internalType: "uint256", name: "_reserveB", type: "uint256" },
+      { internalType: "uint256", name: "_totalSupply", type: "uint256" },
     ],
     stateMutability: "view",
     type: "function",
   },
   {
     inputs: [
-      {
-        internalType: "address",
-        name: "tokenA",
-        type: "address",
-      },
-      {
-        internalType: "address",
-        name: "tokenB",
-        type: "address",
-      },
+      { internalType: "address", name: "", type: "address" },
+      { internalType: "address", name: "", type: "address" },
     ],
     name: "getPrice",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "price",
-        type: "uint256",
-      },
-    ],
+    outputs: [{ internalType: "uint256", name: "price", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "address", name: "", type: "address" }],
+    name: "liquidityBalances",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
     stateMutability: "view",
     type: "function",
   },
   {
     inputs: [
-      {
-        internalType: "bytes32",
-        name: "",
-        type: "bytes32",
-      },
-    ],
-    name: "pools",
-    outputs: [
-      {
-        internalType: "address",
-        name: "tokenA",
-        type: "address",
-      },
-      {
-        internalType: "address",
-        name: "tokenB",
-        type: "address",
-      },
-      {
-        internalType: "uint256",
-        name: "reserveA",
-        type: "uint256",
-      },
-      {
-        internalType: "uint256",
-        name: "reserveB",
-        type: "uint256",
-      },
-      {
-        internalType: "uint256",
-        name: "totalSupply",
-        type: "uint256",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "address",
-        name: "tokenA",
-        type: "address",
-      },
-      {
-        internalType: "address",
-        name: "tokenB",
-        type: "address",
-      },
-      {
-        internalType: "uint256",
-        name: "liquidity",
-        type: "uint256",
-      },
-      {
-        internalType: "uint256",
-        name: "amountAMin",
-        type: "uint256",
-      },
-      {
-        internalType: "uint256",
-        name: "amountBMin",
-        type: "uint256",
-      },
-      {
-        internalType: "address",
-        name: "to",
-        type: "address",
-      },
-      {
-        internalType: "uint256",
-        name: "deadline",
-        type: "uint256",
-      },
+      { internalType: "address", name: "", type: "address" },
+      { internalType: "address", name: "", type: "address" },
+      { internalType: "uint256", name: "liquidity", type: "uint256" },
+      { internalType: "uint256", name: "amountAMin", type: "uint256" },
+      { internalType: "uint256", name: "amountBMin", type: "uint256" },
+      { internalType: "address", name: "to", type: "address" },
+      { internalType: "uint256", name: "deadline", type: "uint256" },
     ],
     name: "removeLiquidity",
     outputs: [
-      {
-        internalType: "uint256",
-        name: "amountA",
-        type: "uint256",
-      },
-      {
-        internalType: "uint256",
-        name: "amountB",
-        type: "uint256",
-      },
-    ],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "uint256",
-        name: "amountIn",
-        type: "uint256",
-      },
-      {
-        internalType: "uint256",
-        name: "amountOutMin",
-        type: "uint256",
-      },
-      {
-        internalType: "address[]",
-        name: "path",
-        type: "address[]",
-      },
-      {
-        internalType: "address",
-        name: "to",
-        type: "address",
-      },
-      {
-        internalType: "uint256",
-        name: "deadline",
-        type: "uint256",
-      },
-    ],
-    name: "swapExactTokensForTokens",
-    outputs: [
-      {
-        internalType: "uint256[]",
-        name: "amounts",
-        type: "uint256[]",
-      },
+      { internalType: "uint256", name: "amountA", type: "uint256" },
+      { internalType: "uint256", name: "amountB", type: "uint256" },
     ],
     stateMutability: "nonpayable",
     type: "function",
   },
   {
     inputs: [],
-    name: "getReserves",
-    outputs: [
-      {
-        internalType: "uint256",
-        name: "reserveA",
-        type: "uint256",
-      },
-      {
-        internalType: "uint256",
-        name: "reserveB",
-        type: "uint256",
-      },
+    name: "reserveA",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "reserveB",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "uint256", name: "amountIn", type: "uint256" },
+      { internalType: "uint256", name: "amountOutMin", type: "uint256" },
+      { internalType: "address[]", name: "path", type: "address[]" },
+      { internalType: "address", name: "to", type: "address" },
+      { internalType: "uint256", name: "deadline", type: "uint256" },
     ],
+    name: "swapExactTokensForTokens",
+    outputs: [
+      { internalType: "uint256[]", name: "amounts", type: "uint256[]" },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "tokenA",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "tokenB",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "totalSupply",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
     stateMutability: "view",
     type: "function",
   },
@@ -570,7 +325,7 @@ export const ERC20_ABI = [
 
 // Contract addresses
 export const CONTRACTS = {
-  SIMPLE_SWAP: "0xaB087FCf85c69Ea7a43dA62bd0f5364110E97C67" as Address,
+  SIMPLE_SWAP: "0xb76D7A08969612e89c5F691215d978d95B1d9cA1" as Address,
   KAIZEN_COIN: "0xb3aD7bC1be272837aD94b1aabebf427b2Be47D9A" as Address,
   YUREI_COIN: "0xcfBaCf4EC0F0676445ed0d41A99b22790F78a46d" as Address,
   VERIFICATION: "0x9f8f02dab384dddf1591c3366069da3fb0018220" as Address,
